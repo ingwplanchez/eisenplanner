a80b69e4a7a3f93c191e134e90b69aaacff8317e
Wilmer Planchez <ing.wplanchez@gmail.com>
2025-07-11 12:35:00 -0400

    Modified: Se actualizan los Formularios para la Matriz de Eisenhower

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ class Task(db.Model):
 # 1. Detén el servidor Flask (Ctrl+C).
 # 2. ELIMINA el archivo 'todo.db' de tu proyecto.
 # 3. DESCOMENTA las siguientes líneas:
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+    # db.create_all()
 # 4. Ejecuta 'python app.py' UNA SOLA VEZ.
 # 5. VUELVE A COMENTAR las líneas 'db.create_all()'.
 # Esto asegura que tu base de datos tenga las nuevas columnas 'is_urgent' y 'is_important'.
