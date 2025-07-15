3119ba6dd9175560f5d8d89e31d9e83455a8b133
Wilmer Planchez <ing.wplanchez@gmail.com>
2025-07-14 22:54:10 -0400

    Modified: Configuración para acceder a la aplicación desde otro dispositivo conectado a la red local

--- app.py
+++ app.py
@@ -217,4 +217,4 @@ def update_task(task_id):
                              important=request.form.get('current_important_filter')))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
